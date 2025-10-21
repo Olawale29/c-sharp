67e38985309100aa1427a645b636d0618f2d63f2
Olawale29 <oyegunleemmanuel39@gmail.com>
2025-10-21 07:31:23 +0100

    Cleaned up repository and added .gitignore

--- Day 3/Program.cs
+++ Day 3/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace HelloWorld
 {
@@ -6,124 +8,237 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            //Data Types
-            //int, string, long, float, double, bool, char
 
-            //The int data type can store whole numbers from -2147483648 to 2147483647
-            int myNum = 100000;
-            Console.WriteLine(myNum);
+            //Console.WriteLine("What is your name ");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("What is your age ");
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine($"Hello {name}, you are {age} years old!");
 
-            long myNumber = 150000000000000000L;
-            Console.WriteLine(myNumber);
+            //Console.WriteLine("Please input first number ");
+            //int number1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Please enter second number ");
+            //int number2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine($"Addition of the two numbers is {number1 + number2}");
+            //Console.WriteLine($"Subtraction of the two numbers is {number1 - number2}");
+            //Console.WriteLine($"Multiplication of the two numbers is {number1 * number2}");
+            //Console.WriteLine($"Division of the two numbers is {number1 / number2}");
 
-            //You should use a floating point type whenever you need a number with a decimal, such as 9.99 or 3.14515.
-            //Note that you should end the value with an "F" for floats and "D" for doubles:
-            float floatedNumber = 7.89F;
-            Console.WriteLine(floatedNumber);
+            //Console.WriteLine("What is your age");
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //int age_in_months = age * 12;
+            //int age_in_weeks = age * 52;
+            //int age_in_days = age * 365;
+            //Console.WriteLine($"You are {age_in_months} months old");
+            //Console.WriteLine($"You are {age_in_weeks} weeks old");
+            //Console.WriteLine($"You are {age_in_days} days old");
 
-            double doubleNumber = 19.99D;
-            Console.WriteLine(doubleNumber);
-
-            //The precision of a floating point value indicates
-            //how many digits the value can have after the decimal point. The precision of float is only six
-            //or seven decimal digits, while double variables have a precision of about 15 digits. Therefore
-            //it is safer to use double for most calculations.
-
-            float f1 = 35e3F;
-            double d1 = 12E4D;
-            Console.WriteLine(f1);
-            Console.WriteLine(d1);
-
-            //Boolean
-            bool isCSharpFun = true;
-            bool isFishTasty = false;
-            Console.WriteLine(isCSharpFun);   // Outputs True
-            Console.WriteLine(isFishTasty);   // Outputs False
-
-            //Char
-            char myGrade = 'B';
-            Console.WriteLine(myGrade);
-
-            //string
-            string my_name = "Emmanuel"; //string values must be sorrounded with double quotes 
-            Console.WriteLine(my_name);
+            //Console.WriteLine("What is your temperature in celsius");
+            //double celsius = Convert.ToDouble(Console.ReadLine());
+            //double F = (celsius * 9 / 5) + 32;
+            //Console.WriteLine($"Your temperature is {F} degree Fahrenheit");
 
 
 
-            //Type Casting
-            //Implicit casting - converting a smaller type to a larger type size automatically
-            //Explicit casting - conconverting a smaller type to a larger type size manually
+
+            //Console.WriteLine("Input first number");
+            //double number1 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Input second number");
+            //double number2 = Convert.ToDouble(Console.ReadLine());
+            //if(number1 > number2)
+            //{
+            //    Console.WriteLine($"The first number {number1} is greater than the second number {number2}");
+            //}else if(number1 < number2)
+            //{
+            //    Console.WriteLine($"The second number {number2} is greater than the first number {number1}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Both numbers are equals to each other");
+            //}
 
 
-            //Implicit
-            int myInt = 9;
-            double myDouble = myInt;
-            Console.WriteLine(myDouble);
-            Console.WriteLine(myInt);
+            //Console.WriteLine("What is the width of the rectangle");
+            //double width = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("What is the height of the rectangle");
+            //double height = Convert.ToDouble(Console.ReadLine());
+            //double Area = width * height;
+            //Console.WriteLine($"The Area of the rectangle is {Area}");
+
+            int score = 0;
+            Console.WriteLine("What is 10 + 5?");
+            int answer1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What is 5 * 4?");
+            int answer2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What is 60 - 20?");
+            int answer3 = Convert.ToInt32(Console.ReadLine());
 
 
-            //Explicit
-            double myDoubles = 9.83;
-            int myInteger = (int)myDoubles;
-            Console.WriteLine(myDoubles);
-            Console.WriteLine(myInteger);
+            if(answer1 == 15 && answer2 == 20 && answer3 == 40 )
+            {
+                score += 30;
+      
+            }else if(answer1 != 15 && answer2 == 20 && answer3 == 40)
+            { 
+                score += 20;
+              
+            }else if (answer1 != 15 && answer2 != 20 && answer3 == 40)
+            {
+                score += 10;
+               
+            }
+            else
+            {
+                score = 0;
+              
+            }
 
-            //Type Conversion methods
-            int myInts = 10;
-            double myDoubless = 5.25;
-            bool myBool = true;
+            Console.WriteLine($"Your result is {score}/30");
 
-            Console.WriteLine(Convert.ToString(myInts));    // convert int to string
-            Console.WriteLine(Convert.ToDouble(myInt));    // convert int to double
-            Console.WriteLine(Convert.ToInt32(myDoubless));  // convert double to int
-            Console.WriteLine(Convert.ToString(myBool));   // convert bool to string
 
-            //User Input
-            Console.WriteLine("What is your age");
-            int age  = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Your age is " + age);
 
-            //operations
-            int x = 100 + 50;
-            int sum1 = 100 + 50;
-            int sum2 = 100 + sum1;
-            int sum3 = sum1 + sum2;
-            Console.WriteLine(sum1);
-            Console.WriteLine(sum2);
-            Console.WriteLine(sum3);
-            Console.WriteLine(x);
+     
 
-            //Arithmetic operaation
-            //+ - Addition
-            //- Subtraction
-            //* - Multiplication
-            // / - Division
-            // % - Modules - returns the division remainder
-            // ++ increment
-            //-- decrement
+            
 
-            int z = 10;
-            z += 5;
-            Console.WriteLine(z);
+            
+            
 
-            //assignment operatiord
 
-            //+=
-            //-=
-            //*=
-            ///=
-            //%=
-            //&=
-            //|=
-            //^=
-            //>>=
-            //<<=
 
-            //comparison operators
-            int d = 5;
-            int c = 3;
-            Console.WriteLine(d > c);
 
+
+
+            ////Data Types
+            ////int, string, long, float, double, bool, char
+
+            ////The int data type can store whole numbers from -2147483648 to 2147483647
+            //int myNum = 100000;
+            //Console.WriteLine(myNum);
+
+            //long myNumber = 150000000000000000L;
+            //Console.WriteLine(myNumber);
+
+            ////You should use a floating point type whenever you need a number with a decimal, such as 9.99 or 3.14515.
+            ////Note that you should end the value with an "F" for floats and "D" for doubles:
+            //float floatedNumber = 7.89F;
+            //Console.WriteLine(floatedNumber);
+
+            //double doubleNumber = 19.99D;
+            //Console.WriteLine(doubleNumber);
+
+            ////The precision of a floating point value indicates
+            ////how many digits the value can have after the decimal point. The precision of float is only six
+            ////or seven decimal digits, while double variables have a precision of about 15 digits. Therefore
+            ////it is safer to use double for most calculations.
+
+            //float f1 = 35e3F;
+            //double d1 = 12E4D;
+            //Console.WriteLine(f1);
+            //Console.WriteLine(d1);
+
+            ////Boolean
+            //bool isCSharpFun = true;
+            //bool isFishTasty = false;
+            //Console.WriteLine(isCSharpFun);   // Outputs True
+            //Console.WriteLine(isFishTasty);   // Outputs False
+
+            ////Char
+            //char myGrade = 'B';
+            //Console.WriteLine(myGrade);
+
+            ////string
+            //string my_name = "Emmanuel"; //string values must be sorrounded with double quotes 
+            //Console.WriteLine(my_name);
+
+
+
+            ////Type Casting
+            ////Implicit casting - converting a smaller type to a larger type size automatically
+            ////Explicit casting - conconverting a smaller type to a larger type size manually
+
+
+            ////Implicit
+            //int myInt = 9;
+            //double myDouble = myInt;
+            //Console.WriteLine(myDouble);
+            //Console.WriteLine(myInt);
+
+
+            ////Explicit
+            //double myDoubles = 9.83;
+            //int myInteger = (int)myDoubles;
+            //Console.WriteLine(myDoubles);
+            //Console.WriteLine(myInteger);
+
+            ////Type Conversion methods
+            //int myInts = 10;
+            //double myDoubless = 5.25;
+            //bool myBool = true;
+
+            //Console.WriteLine(Convert.ToString(myInts));    // convert int to string
+            //Console.WriteLine(Convert.ToDouble(myInt));    // convert int to double
+            //Console.WriteLine(Convert.ToInt32(myDoubless));  // convert double to int
+            //Console.WriteLine(Convert.ToString(myBool));   // convert bool to string
+
+            ////User Input
+            //Console.WriteLine("What is your age");
+            //int age  = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Your age is " + age);
+
+            ////operations
+            //int x = 100 + 50;
+            //int sum1 = 100 + 50;
+            //int sum2 = 100 + sum1;
+            //int sum3 = sum1 + sum2;
+            //Console.WriteLine(sum1);
+            //Console.WriteLine(sum2);
+            //Console.WriteLine(sum3);
+            //Console.WriteLine(x);
+
+            ////Arithmetic operaation
+            ////+ - Addition
+            ////- Subtraction
+            ////* - Multiplication
+            //// / - Division
+            //// % - Modules - returns the division remainder
+            //// ++ increment
+            ////-- decrement
+
+            //int z = 10;
+            //z += 5;
+            //Console.WriteLine(z);
+
+            ////assignment operatiord
+
+            ////+=
+            ////-=
+            ////*=
+            /////=
+            ////%=
+            ////&=
+            ////|=
+            ////^=
+            ////>>=
+            ////<<=
+
+            ////comparison operators
+            //int d = 5;
+            //int c = 3;
+            //Console.WriteLine(d > c);
+
+            ////more examples are
+            ////== Equal to x == y
+            ////!= Not equal x != y
+            ////> Greater than x > y
+            ////< Less than x<y
+            ////>= Greater than or equal to    x >= y
+            ////<=
+
+            ////Logical operators
+            ////As with comparison operators, you can also test for True or False values with logical operators.
+
+            ////examples are &&- Logical AND, || - Logical OR, ! - Logical NOT
 
 
 
---
+++ Day 4/Program.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //c# math
+            Console.WriteLine(Math.Max(5, 10));
+            Console.WriteLine(Math.Min(20, 50));
+            Console.WriteLine(Math.Sqrt(64));
+            Console.WriteLine(Math.Abs(-4.978));
+            Console.WriteLine(Math.Round(9.99));
+
+            //C# strings
+            string greetings = "Hello";
+            string greetings2 = "Nice to meet you";
+
+            string txt = "ABCDEFGHIJKLMIOPQRSTUVWZYZ";
+            Console.WriteLine($"The length of the text is {txt.Length}");
+
+            //There are many string methods available, for example ToUpper() and ToLower(), which returns a copy of the string converted to uppercase or lowercase:
+            Console.WriteLine(greetings.ToUpper());
+            Console.WriteLine(greetings2.ToUpper());
+
+            //string concatination
+            string firstname = "Oyegunle ";
+            string lastname = "Emmanuel";
+            string name = firstname + lastname;
+            Console.WriteLine(name);
+
+            //string.Concat
+            string first_name = "Oyegunle ";
+            string last_name = "Emmanuel";
+            string full_name = String.Concat(first_name, last_name);
+            Console.WriteLine(full_name);
+
+            int a = 10;
+            int b = 20;
+            int c = a + b;  // c will be 30 (an integer/number)
+
+            string x = "10";
+            string y = "20";
+            string z = x + y;  // z will be 1020 (a string)
+
+            //string interpolation
+            string firstName = "John";
+            string lastName = "Doe";
+            string fullName = $"My full name is: {firstName} {lastName}";
+            Console.WriteLine(fullName);
+
+            //Access string
+            //string myString = "Hello";
+            //Console.WriteLine(myString.IndexOf("e"));
+
+            string myString = "Hello";
+            Console.WriteLine(myString[0]);
+            Console.WriteLine(myString.IndexOf("e"));
+
+
+            string user_name = "John Doe";
+
+            // Location of the letter D
+            int charPos = user_name.IndexOf("D");
+
+            // Get last name
+            string last_names = name.Substring(charPos);
+
+            // Print the result
+            Console.WriteLine(last_names);
+
+
+            //c# special characters
+            string txts = "We are the so-called \"Vikings\" from the north.";
+            Console.WriteLine(txts);
+
+            string text = "It\'s alright.";
+            Console.WriteLine(text);
+
+            string texts = "The character \\ is called backslash.";
+            Console.WriteLine(texts);
+
+            //c# booleans
+            bool isCSharpFun = true;
+            bool isFishTasty = false;
+            Console.WriteLine(isCSharpFun);   // Outputs True
+            Console.WriteLine(isFishTasty);
+            Console.WriteLine(10 > 9);
+
+            //int myAge = 25;
+            //int votingAge = 18;
+            //Console.WriteLine(myAge >= votingAge);
+
+            int myAge = 17;
+            int votingAge = 18;
+
+            if (myAge >= votingAge)
+            {
+                Console.WriteLine("Old enough to vote!");
+            }
+            else
+            {
+                Console.WriteLine("Not old enough to vote.");
+            }
+
+            //If else
+            int m = 20;
+            int n = 18;
+            if (m > n)
+            {
+                Console.WriteLine("m is greater than n");
+            }
+
+            //int time = 20;
+            //if (time < 18)
+            //{
+            //    Console.WriteLine("Good day.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Good evening.");
+            //}
+
+            int time = 22;
+            if (time < 10)
+            {
+                Console.WriteLine("Good morning.");
+            }
+            else if (time < 20)
+            {
+                Console.WriteLine("Good day.");
+            }
+            else
+            {
+                Console.WriteLine("Good evening.");
+            }
+
+            //shot hand
+            int times = 20;
+            string result = (times < 18) ? "Good day." : "Good evening.";
+            Console.WriteLine(result);
+
+            //switch cases
+            int day = 4;
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+            }
+
+            int dayx = 4;
+            switch (dayx)
+            {
+                case 6:
+                    Console.WriteLine("Today is Saturday.");
+                    break;
+                case 7:
+                    Console.WriteLine("Today is Sunday.");
+                    break;
+                default:
+                    Console.WriteLine("Looking forward to the Weekend.");
+                    break;
+            }
+
+            //while loop
+
+            //int i = 0;
+            //while(i< 5)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+            //do-while
+
+            //do
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+            //while (i < 5);
+
+            //for (int i = 1; i <= 11; i = i + 2)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //nested loop
+            for (int i = 1; i <= 2; ++i)
+            {
+                Console.WriteLine("Outer: " + i);  // Executes 2 times
+
+                // Inner loop
+                for (int j = 1; j <= 3; j++)
+                {
+                    Console.WriteLine(" Inner: " + j); // Executes 6 times (2 * 3)
+                }
+            }
+
+            string[] cars = {"volvo", "tesla", "BMW", "Mazda"};
+            foreach(string car in cars)
+            {
+                Console.WriteLine(car);
+            }
+
+            //break statement
+            //for(int i = 0; i < 10; i++)
+            //{
+            //    if(i == 4)
+            //    {
+            //        break;
+            //    }
+            //    Console.WriteLine(i);
+            //}
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    if (i == 4)
+            //    {
+            //        continue;
+            //    }
+            //    Console.WriteLine(i);
+            //}
+
+            int s = 0;
+            while(s < 10)
+            {
+                Console.WriteLine(s);
+                s++;
+
+                if(s == 4)
+                {
+                    break;
+                }
+            }
+
+            int d = 0;
+            while(d < 10)
+            {
+                Console.WriteLine(d);
+                d++;
+                if(d == 4)
+                {
+                    continue;
+                }
+            }
+
+
+            string car1 = "Volvo";
+            string car2 = "BMW";
+            string car3 = "Ford";
+
+            string[] carX = {car1, car2, car3};
+            Console.WriteLine(carX[0]);
+
+
+            //Arrays hold multiple values of the same type.
+
+            //They are indexed starting from 0.
+
+            //Use.Length to get the number of items.
+
+            //Loop through them with for or foreach.
+
+        }
+    }
+}
